a0d94504c88aadf63dbdc5bb216ce3fd41b46775
casa <casatwy@msn.com>
2018-02-27 16:52:40 +0800

    modification

--- Podfile
+++ Podfile
@@ -1,11 +1,11 @@
-# Uncomment the next line to define a global platform for your project
-# platform :ios, '9.0'
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
 
 target 'CTNetworking' do
-  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # Uncomment this line if you're using Swift or would like to use dynamic frameworks
   # use_frameworks!
-
-  # Pods for CTNetworking
+ 
   pod "AFNetworking"
+
 
 end
---
+++ upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VersionString=`grep -E 's.version.*=' CTNetworking.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' CTNetworking.podspec | cut -d : -f1`
+
+git add .
+git commit -am modification
+git pull origin master --tags
+
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" CTNetworking.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+say "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+cd ~/.cocoapods/repos/BLRepositories && git pull origin master && cd - && pod repo push BLRepositories CTNetworking.podspec --verbose --allow-warnings --use-libraries
